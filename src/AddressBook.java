2b7b04c7229f24e208d1d8c6c3edd62a9058a711
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-03 12:47:58 +0530

    ADDED : code to add a contact in address book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
+
+    static final ArrayList<Contact> book=new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
+        Scanner sc=new Scanner(System.in);
+        Contact person=new Contact();
+        System.out.println("Enter first name : ");
+        String firstName=sc.nextLine();
+        System.out.println("Enter last name : ");
+        String lastName=sc.nextLine();
+        System.out.println("Enter address : ");
+        String address=sc.nextLine();
+        System.out.println("Enter city : ");
+        String city=sc.nextLine();
+        System.out.println("Enter state : ");
+        String state=sc.nextLine();
+        System.out.println("Enter pin : ");
+        int pin=sc.nextInt();
+        System.out.println("Enter phone number : ");
+        long phoneNumber=sc.nextLong();
+        System.out.println("Enter email : ");
+        String email=sc.nextLine();
+
+        person.firstName=firstName;
+        person.lastName= lastName;
+        person.address=address;
+        person.city=city;
+        person.state=state;
+        person.pin=pin;
+        person.phoneNumber=phoneNumber;
+        person.email=email;
+        book.add(person);
     }
 }
---
+++ src/Contact.java
@@ -0,0 +1,12 @@
+public class Contact {
+
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int pin;
+    long phoneNumber;
+    String email;
+
+}
