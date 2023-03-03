3d0c5e71716be20624d2b016456e5834d3d25b45
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-03 13:26:10 +0530

    REFACTORED : code to initialize contact details

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,41 +4,9 @@ import java.util.Scanner;
 public class AddressBook {
 
     static final ArrayList<Contact> book=new ArrayList<>();
-    static Scanner sc=new Scanner(System.in);
-
-    public static Contact createContact(){
-        Contact person=new Contact();
-        System.out.println("New Entry : ");
-        System.out.print("Enter first name : ");
-        String firstName=sc.nextLine();
-        System.out.print("Enter last name : ");
-        String lastName=sc.nextLine();
-        System.out.print("Enter address : ");
-        String address=sc.nextLine();
-        System.out.print("Enter city : ");
-        String city=sc.nextLine();
-        System.out.print("Enter state : ");
-        String state=sc.nextLine();
-        System.out.print("Enter pin : ");
-        int pin=sc.nextInt();
-        System.out.print("Enter phone number : ");
-        long phoneNumber=sc.nextLong();
-        System.out.print("Enter email : ");
-        String email=sc.nextLine();
-        sc.nextLine();
-
-        person.firstName=firstName;
-        person.lastName= lastName;
-        person.address=address;
-        person.city=city;
-        person.state=state;
-        person.pin=pin;
-        person.phoneNumber=phoneNumber;
-        person.email=email;
-        return person;
-    }
 
     public static void editContact(){
+        Scanner sc=new Scanner(System.in);
         System.out.print("Enter first name : ");
         String firstName=sc.nextLine();
         System.out.print("Enter last name : ");
@@ -53,8 +21,8 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        Contact person1=createContact();
-        Contact person2=createContact();
+        Contact person1=new Contact();
+        Contact person2=new Contact();
         book.add(person1);
         book.add(person2);
         editContact();
--- src/Contact.java
+++ src/Contact.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Contact {
 
     String firstName;
@@ -8,5 +10,27 @@ public class Contact {
     int pin;
     long phoneNumber;
     String email;
+
+    public Contact(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("New Entry : ");
+        System.out.print("Enter first name : ");
+        firstName=sc.nextLine();
+        System.out.print("Enter last name : ");
+        lastName=sc.nextLine();
+        System.out.print("Enter address : ");
+        address=sc.nextLine();
+        System.out.print("Enter city : ");
+        city=sc.nextLine();
+        System.out.print("Enter state : ");
+        state=sc.nextLine();
+        System.out.print("Enter pin : ");
+        pin=sc.nextInt();
+        System.out.print("Enter phone number : ");
+        phoneNumber=sc.nextLong();
+        System.out.print("Enter email : ");
+        email=sc.nextLine();
+        sc.nextLine();
+    }
 
 }
