8e2c225b249dd496357ebdb4b411e5ce1085b8e7
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-03 13:50:49 +0530

    REFACTORED : code to delete contact

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class AddressBook {
 
     static final ArrayList<Contact> book = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
     public static void editContact() {
-        Scanner sc = new Scanner(System.in);
         System.out.print("Enter first name : ");
         String firstName = sc.nextLine();
         System.out.print("Enter last name : ");
@@ -19,43 +19,64 @@ public class AddressBook {
                 switch (choice) {
                     case 1 -> {
                         System.out.print("Enter new first name : ");
-                        c.firstName=sc.nextLine();
+                        c.firstName = sc.nextLine();
                     }
                     case 2 -> {
                         System.out.print("Enter new last name : ");
-                        c.lastName=sc.nextLine();
+                        c.lastName = sc.nextLine();
                     }
                     case 3 -> {
                         System.out.print("Enter new address : ");
-                        c.address=sc.nextLine();
+                        c.address = sc.nextLine();
                     }
                     case 4 -> {
-                        System.out.println("Enter new city : ");
-                        c.city=sc.nextLine();
+                        System.out.print("Enter new city : ");
+                        c.city = sc.nextLine();
                     }
                     case 5 -> {
-                        System.out.println("Enter new state : ");
-                        c.state=sc.nextLine();
+                        System.out.print("Enter new state : ");
+                        c.state = sc.nextLine();
                     }
                     case 6 -> {
-                        System.out.println("Enter new pin : ");
-                        c.pin=sc.nextInt();
+                        System.out.print("Enter new pin : ");
+                        c.pin = sc.nextInt();
                     }
                     case 7 -> {
-                        System.out.println("Enter new phone number : ");
-                        c.phoneNumber=sc.nextLong();
+                        System.out.print("Enter new phone number : ");
+                        c.phoneNumber = sc.nextLong();
                     }
                 }
+                sc.nextLine();
             }
         }
         System.out.println("Contact doesn't exist in address book");
     }
 
+    public static void deleteContact() {
+        System.out.print("Enter first name : ");
+        String firstName = sc.nextLine();
+        System.out.print("Enter last name : ");
+        String lastName = sc.nextLine();
+
+        for (Contact c : book) {
+            if (c.firstName.equals(firstName) && c.lastName.equals(lastName)) {
+                book.remove(c);
+            }
+        }
+        System.out.println("Contact deleted");
+    }
+
+
     public static void main(String[] args) {
-        Contact person1 = new Contact();
-        Contact person2 = new Contact();
-        book.add(person1);
-        book.add(person2);
-        editContact();
+        System.out.print("Enter choice : (1)Add contact (2)edit contact (3)delete contact -> ");
+        int choice= sc.nextInt();
+        switch(choice){
+            case 1 -> {
+                Contact person=new Contact();
+                book.add(person);
+            }
+            case 2 -> editContact();
+            case 3 -> deleteContact();
+        }
     }
 }
