70d344ff15dc94d73c4598b7801adfcd3fe3adf8
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-03-07 18:18:08 +0530

    REFACTORED : added getters and setters and made methods non static

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -6,14 +6,13 @@ import java.util.Scanner;
 public class AddressBook {
 
     static Scanner sc = new Scanner(System.in);
-    static final Map<String, ArrayList<Contact>> DICTIONARY = new HashMap<>();
-    ArrayList<Contact> book;
+    Map<String, ArrayList<Contact>> DICTIONARY;
 
     public AddressBook() {
-        book = new ArrayList<>();
+        DICTIONARY=new HashMap<>();
     }
 
-    public static void editContact(ArrayList<Contact> book) {
+    public  void editContact(ArrayList<Contact> book) {
         final int FIRST_NAME = 1;
         final int LAST_NAME = 2;
         final int ADDRESS = 3;
@@ -30,42 +29,42 @@ public class AddressBook {
         String lastName = sc.next();
 
         for (int i=0; i< book.size(); i++) {
-            if (book.get(i).firstName.equals(firstName) && book.get(i).lastName.equals(lastName)) {
+            if (book.get(i).getFirstName().equals(firstName) && book.get(i).getLastName().equals(lastName)) {
                 sc.nextLine();
                 System.out.print("What do you want to edit? (1)first name (2)last name (3)address (4)city (5)state (6)pin (7)phone number (8)email (9)everything -> ");
                 int choice = sc.nextInt();
                 switch (choice) {
                     case FIRST_NAME -> {
                         System.out.print("Enter new first name : ");
-                        book.get(i).firstName = sc.next();
+                        book.get(i).setFirstName(sc.next());
                     }
                     case LAST_NAME -> {
                         System.out.print("Enter new last name : ");
-                        book.get(i).lastName = sc.next();
+                        book.get(i).setLastName(sc.next());
                     }
                     case ADDRESS -> {
                         System.out.print("Enter new address : ");
-                        book.get(i).address = sc.nextLine();
+                        book.get(i).setAddress(sc.nextLine());
                     }
                     case CITY -> {
                         System.out.print("Enter new city : ");
-                        book.get(i).city = sc.next();
+                        book.get(i).setCity(sc.next());
                     }
                     case STATE -> {
                         System.out.print("Enter new state : ");
-                        book.get(i).state = sc.nextLine();
+                        book.get(i).setState(sc.nextLine());
                     }
                     case PIN -> {
                         System.out.print("Enter new pin : ");
-                        book.get(i).pin = sc.nextInt();
+                        book.get(i).setPin(sc.nextInt());
                     }
                     case PHONE_NUMBER -> {
                         System.out.print("Enter new phone number : ");
-                        book.get(i).phoneNumber = sc.nextLong();
+                        book.get(i).setPhoneNumber(sc.nextLong());
                     }
                     case EMAIL -> {
                         System.out.print("Enter new EMAIL : ");
-                        book.get(i).email = sc.nextLine();
+                        book.get(i).setEmail(sc.nextLine());
                     }
                     case EVERYTHING -> book.set(i, new Contact());
                     default -> System.out.println("Wrong input");
@@ -79,14 +78,14 @@ public class AddressBook {
         System.out.println();
     }
 
-    public static void deleteContact(ArrayList<Contact> book) {
+    public  void deleteContact(ArrayList<Contact> book) {
         System.out.print("Enter first name : ");
         String firstName = sc.next();
         System.out.print("Enter last name : ");
         String lastName = sc.next();
 
         for (Contact c : book) {
-            if (c.firstName.equals(firstName) && c.lastName.equals(lastName)) {
+            if (c.getFirstName().equals(firstName) && c.getLastName().equals(lastName)) {
                 book.remove(c);
                 System.out.println("Contact deleted");
                 System.out.println();
@@ -97,21 +96,21 @@ public class AddressBook {
         System.out.println();
     }
 
-    public static void printAddressBook(ArrayList<Contact> book) {
+    public  void printAddressBook(ArrayList<Contact> book) {
         for (Contact c : book) {
             System.out.println("~~~~~~");
-            System.out.println("Name : " + c.firstName + " " + c.lastName);
-            System.out.println("Address : " + c.address);
-            System.out.println("City : " + c.city);
-            System.out.println("State : " + c.state);
-            System.out.println("Pin : " + c.pin);
-            System.out.println("Phone No : " + c.phoneNumber);
-            System.out.println("Email : " + c.email);
+            System.out.println("Name : " + c.getFirstName() + " " + c.getLastName());
+            System.out.println("Address : " + c.getAddress());
+            System.out.println("City : " + c.getCity());
+            System.out.println("State : " + c.getState());
+            System.out.println("Pin : " + c.getPin());
+            System.out.println("Phone No : " + c.getPhoneNumber());
+            System.out.println("Email : " + c.getEmail());
             System.out.println("~~~~~~");
         }
     }
 
-    public static void operateBook(String bookName, ArrayList<Contact> book) {
+    public  void operateBook(String bookName, ArrayList<Contact> book) {
         final int ADD_CONTACT = 1;
         final int EDIT_CONTACT = 2;
         final int DELETE_CONTACT = 3;
@@ -136,14 +135,14 @@ public class AddressBook {
         }
     }
 
-    public static void printDictionary() {
+    public  void printDictionary() {
         for (Map.Entry<String, ArrayList<Contact>> entry : DICTIONARY.entrySet()) {
             System.out.println("~~~~" + entry.getKey() + "~~~~~");
             printAddressBook(entry.getValue());
         }
     }
 
-    public static void createAddressBook() {
+    public  void createAddressBook() {
         System.out.print("Enter name of address book : ");
         String name = sc.nextLine();
         if (DICTIONARY.containsKey(name)) {
@@ -151,12 +150,12 @@ public class AddressBook {
             System.out.println();
             return;
         }
-        AddressBook address = new AddressBook();
-        DICTIONARY.put(name, address.book);
-        operateBook(name, address.book);
+        ArrayList<Contact> book=new ArrayList<>();
+        DICTIONARY.put(name, book);
+        operateBook(name, book);
     }
 
-    public static void chooseAddressBook() {
+    public  void chooseAddressBook() {
         System.out.print("Enter name of address book : ");
         String name = sc.nextLine();
         if (DICTIONARY.containsKey(name))
@@ -167,19 +166,22 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
+        AddressBook addressBook=new AddressBook();
+
         final int CREATE_ADDRESS_BOOK = 1;
         final int CHOOSE_ADDRESS_BOOK = 2;
         final int PRINT_ALL_BOOKS = 3;
         final int EXIT = 0;
+
         while (true) {
             System.out.println();
             System.out.print("Enter choice : (1)Create new address book (2)Choose an address book (3)Print dictionary (0)Exit : ");
             int choice = sc.nextInt();
             sc.nextLine();
             switch (choice) {
-                case CREATE_ADDRESS_BOOK -> createAddressBook();
-                case CHOOSE_ADDRESS_BOOK -> chooseAddressBook();
-                case PRINT_ALL_BOOKS -> printDictionary();
+                case CREATE_ADDRESS_BOOK -> addressBook.createAddressBook();
+                case CHOOSE_ADDRESS_BOOK -> addressBook.chooseAddressBook();
+                case PRINT_ALL_BOOKS -> addressBook.printDictionary();
                 case EXIT -> {
                     return;
                 }
